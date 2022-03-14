1526d0ea226b464541f585add5f34fabd8cd300d
Sarang Wadode <sarangwadode@gmail.com>
2022-03-14 20:01:14 +0530

    feat: error page added

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,6 @@
 import { Title } from "@mantine/core";
 import { Login } from "./components/login";
+import { Error } from "./components/error";
 
 const routes = [
     {path: '/', element: <Title>Home Page</Title>},
@@ -9,7 +10,7 @@ const routes = [
     {path: '/filtered', element: <Title align="center">Filtered Page</Title>},
     {path: '/deleted', element: <Title align="center">Deleted Page</Title>},
     {path: '/about', element: <Title align="center">About Page</Title>},
-    {path: '*', element: <Title align="center">404 Error</Title>},
+    {path: '*', element: <Error/>},
 ]
 
 export default routes;
