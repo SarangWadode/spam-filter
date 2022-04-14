0517deec28fdfb155cac2cd9acb6696f0e98f940
Sarang Wadode <sarangwadode@gmail.com>
2022-04-14 10:42:41 +0530

    signup added

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,21 +1,25 @@
 import { useForm } from '@mantine/hooks';
+import { useLocation, useNavigate } from 'react-router-dom';
 import {
     TextInput,
     PasswordInput,
     Checkbox,
     Anchor,
     Paper,
-    Title,
-    Text,
     Container,
     Group,
     Button,
+    Tabs,
 } from '@mantine/core';
 
 export function Login() {
+    const router = useLocation();
+    const navigate = useNavigate();
+    const active = (router.pathname === '/login') ? 0 : 1
+
     const form = useForm({
         initialValues: {
-            username: '',
+            email: '',
             password: ''
         },
         validationRules: {
@@ -27,38 +31,62 @@ export function Login() {
             password: 'Incorrect password'
         }
     })
-    return (
-        <Container sx={{margin: 'auto'}} >
-            <Title
-                align="center"
-                sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 900 })}
-            >
-                Admin Login
-            </Title>
-            <Text color="dimmed" size="sm" align="center" mt={5}>
-                Do not have an account yet?{' '}
-                {/* TODO: add route */}
-                <Anchor href="#" size="sm" onClick={(event) => event.preventDefault()}>
-                    Create account
-                </Anchor>
-            </Text>
+    const signup = useForm({
+        initialValues: {
+            username: '',
+            password: '',
+            email: '',
+            confirmpassword: ''
+        },
+        validationRules: {
+            username: (val) => val.length >= 6,
+            password: (val) => val.length >= 6,
+            email: (val) => val.length >= 6,
+            confirmPassword: (confirmpassword, values) => confirmpassword === values.password
+        },
+        errorMessages: {
+            username: 'Should be more than 6 characters',
+            password: 'Incorrect password',
+            email: 'Incorrect email',
+            confirmpassword: 'password missmatched'
+        }
+    })
 
-            <form onSubmit={form.onSubmit(values => form.validate && console.log(values))}>
-                <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-                    <TextInput label="Username" placeholder="username" {...form.getInputProps('username')} required />
-                    <PasswordInput label="Password" placeholder="password" {...form.getInputProps('password')} required mt="md" />
-                    <Group position="apart" mt="md">
-                        <Checkbox label="Remember me" />
-                        {/* TODO: add route */}
-                        <Anchor onClick={(event) => event.preventDefault()} href="#" size="sm">
-                            Forgot password?
-                        </Anchor>
-                    </Group>
-                    <Button type='submit' fullWidth mt="xl">
-                        Login
-                    </Button>
-                </Paper >
-            </form>
+    return (
+        <Container sx={{ margin: 'auto' }} style={{minWidth: '350px'}}>
+            <Tabs tabPadding="sm" grow active={active} onTabChange={(ind) => navigate((ind === 1) ? '/signup' : '/login')}>
+                <Tabs.Tab label='Login'>
+                    <form onSubmit={form.onSubmit(values => form.validate && console.log(values))}>
+                        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+                            <TextInput label="Username" placeholder="username" {...form.getInputProps('username')} required />
+                            <PasswordInput label="Password" placeholder="password" {...form.getInputProps('password')} required mt="md" />
+                            <Group position="apart" mt="md">
+                                <Checkbox label="Remember me" />
+                                {/* TODO: add route */}
+                                <Anchor onClick={(event) => event.preventDefault()} href="#" size="sm">
+                                    Forgot password?
+                                </Anchor>
+                            </Group>
+                            <Button type='submit' fullWidth mt="xl">
+                                Login
+                            </Button>
+                        </Paper >
+                    </form>
+                </Tabs.Tab>
+                <Tabs.Tab label='SignUp'>
+                    <form onSubmit={signup.onSubmit(values => signup.validate && console.log(values))}>
+                        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+                            <TextInput label="Username" placeholder="username" {...signup.getInputProps('username')} required />
+                            <TextInput label="Email" placeholder="email" {...signup.getInputProps('email')} required mt="md" />
+                            <PasswordInput label="Password" placeholder="password" {...signup.getInputProps('password')} required mt="md" />
+                            <PasswordInput label="Confirm Password" placeholder="password" {...signup.getInputProps('confirmPassword')} required mt="md" />
+                            <Button type='submit' fullWidth mt="xl">
+                                SignUp
+                            </Button>
+                        </Paper >
+                    </form>
+                </Tabs.Tab>
+            </Tabs>
         </Container >
     );
 }
