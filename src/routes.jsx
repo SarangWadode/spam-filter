af09c8a2e9d3493cfa9c57bd51ccca4114f51966
Sarang Wadode <sarangwadode@gmail.com>
2022-04-15 12:21:03 +0530

    routes changed and mock apis deleted

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,14 +3,16 @@ import { Login } from "./components/login";
 import { Error } from "./components/error";
 import { Products } from "./components/products";
 import { Reviews } from "./components/reviews";
+import { Deleted } from "./components/deleted";
+
 const routes = [
     {path: '/', element: <Title>Home Page</Title>},
     {path: '/login', element: <Login />},
     {path: '/signup', element: <Login />},
     {path: '/products', element: <Products />},
-    {path: '/reviews', element: <Reviews />},
+    {path: '/products/:pk', element: <Reviews />},
     {path: '/filtered', element: <Title align="center">Filtered Page</Title>},
-    {path: '/deleted', element: <Title align="center">Deleted Page</Title>},
+    {path: '/deleted', element: <Deleted />},
     {path: '/about', element: <Title align="center">About Page</Title>},
     {path: '*', element: <Error/>},
 ]
